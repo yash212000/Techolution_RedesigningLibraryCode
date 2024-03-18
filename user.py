d5f42d4dfe639fbb63e46318bc6e068456085c2a
yash212000 <85987936+yash212000@users.noreply.github.com>
2024-03-18 22:50:17 +0530

    Delete user.py

--- user.py
+++
@@ -1,9 +0,0 @@
-'''This file defines User Class representing users of the library with basic information like name and user ID.'''
-
-class User:
-    def __init__(self, name, user_id): # Initializes new user object with name and user ID
-        self.name = name
-        self.user_id = user_id
-
-    def __repr__(self): # Provides a string representation of user object, used while printing.
-        return f"User(name={self.name}, user_id={self.user_id})"
